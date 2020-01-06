5bcfd0f13365e4dc31df7130cf1801ba234c206a
Rajendran Kumaran <rajendrank@virtusa.com>
2020-01-06 11:56:29 +0530

    package changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Webpage';
+
+  navBarOpen = false;
+
+  toggle() {
+    this.navBarOpen = !this.navBarOpen;
+  }
 }
