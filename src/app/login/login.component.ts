049ff2ae5036b848b49a3d6db89ac5740310f1a3
Rajendran Kumaran <rajendrank@virtusa.com>
2020-01-08 21:17:58 +0530

    successful naviagtion on valid user login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Directive, Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { FormGroup } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 import { Login } from './login';
 import { UserDataService } from '../services/user-data.service';
 import { UserDataClass } from '../services/user-data-class';
+import { map, filter, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -14,12 +16,13 @@ import { UserDataClass } from '../services/user-data-class';
 export class LoginComponent implements OnInit {
 
   private usersData : UserDataClass[] = [];
+  private unknownUser : boolean = true;
   user : Login = {
     username : "",
     password : ""
   };
 
-  constructor(private _userDataService : UserDataService) { }
+  constructor(private _userDataService : UserDataService, private _route : Router) { }
 
   ngOnInit() {
     this.getUsersData();
@@ -30,10 +33,23 @@ export class LoginComponent implements OnInit {
       (data) => {
         this.usersData = data;
         console.log(this.usersData);
-      },      
+      },
       error => {
+        alert("Error getting data from the server, Please refresh to continue");
         console.log(error);
       })
+  }
+
+  loginSubmit(data) {
+    console.log(data.value);
+    if (this.usersData.filter( usDt => {
+      if (data.value.username == usDt.username && data.value.password == usDt.password)
+        return usDt; }).length > 0) {
+      this._route.navigate(['/Home']);
+    }
+    else {
+      alert("Not Exist");
+    }
   }
 
 }
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,4 +1,4 @@
-export class Login {
+export interface Login {
   username : string;
   password : string;
 }
